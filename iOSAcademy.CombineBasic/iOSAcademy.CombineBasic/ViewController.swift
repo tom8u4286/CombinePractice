a5022c932e86560704ee63f9476874d41cc2aa5d
Tomtom <Tomtom_Chu@everrich.com.tw>
2023-04-05 16:04:02 +0800

    新增 iOSAcademy Combine練習Project，使用PassthroughSubject將Cell的點按事件回傳給VC

--- iOSAcademy.CombineBasic/iOSAcademy.CombineBasic/ViewController.swift
+++ iOSAcademy.CombineBasic/iOSAcademy.CombineBasic/ViewController.swift
@@ -9,7 +9,40 @@ import UIKit
 import Combine
 
 class MyCustomTableCell: UITableViewCell {
+    private let button: UIButton = {
+        let button = UIButton()
+        button.setTitle("button", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemPink
+        return button
+    }()
     
+    /// 在Cell這邊實作一個PassthroughSubject的Publisher。
+    /// <String, Never>表示我們publish的資料型別為String，並且不會有錯誤發生。
+    ///
+    /// -Authors: Tomtom Chu
+    /// -Date: 2023.4.5
+    let action = PassthroughSubject<String, Never>()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(button)
+        
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
+    
+    @objc private func buttonTapped(){
+        action.send("Button was tapped!")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        button.frame = CGRect(x: 10, y: 3, width: contentView.frame.size.width - 20, height: contentView.frame.size.height - 6)
+    }
 }
 
 
@@ -23,7 +56,14 @@ class ViewController: UIViewController, UITableViewDataSource {
     
     private var models = [String]()
     
-    var observer: AnyCancellable?
+    /// 由於有可能會有非常多個Cell，
+    /// 每一個Cell都會有一組Publisher與Observer，
+    /// 所以這裡常常會使用Array來存放這些Observers。
+    ///
+    /// -Authors: Tomtom Chu
+    /// -Date: 2023.4.5
+    /// var observer: AnyCancellable?
+    var observers: [AnyCancellable] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,7 +72,7 @@ class ViewController: UIViewController, UITableViewDataSource {
         tableView.dataSource = self
         tableView.frame = view.bounds
         
-        observer = APICaller.shared.fetchCompanies()
+        APICaller.shared.fetchCompanies()
             // receive:讓Publisher丟出來的事件，丟到某一個執行緒中
             .receive(on: DispatchQueue.main)
             .sink(receiveCompletion: { completion in
@@ -47,7 +87,8 @@ class ViewController: UIViewController, UITableViewDataSource {
             /// 由於先前已經使用receive，將資料丟給main執行緒，
             /// 這裡不需要再將reload的工作，用dispatchQueue丟給主執行緒。
             self?.tableView.reloadData()
-        })
+        // 利用.store將這個observer存入ovservers陣列中
+        }).store(in: &observers)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -58,7 +99,17 @@ class ViewController: UIViewController, UITableViewDataSource {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? MyCustomTableCell else {
             fatalError()
         }
-        cell.textLabel?.text = models[indexPath.row]
+        
+        /// sink會回傳的是一個observer (subscriber)，
+        /// 因此我們可以將這個物件(obserser)用變數存起來，方便管理。
+        ///
+        /// -Authors: Tomtom Chu
+        /// -Date: 2023.4.5
+        cell.action.sink { string in
+            print(string)
+        // 利用.store將這個observer存入ovservers陣列中
+        }.store(in: &observers)
+        
         return cell
     }
 
